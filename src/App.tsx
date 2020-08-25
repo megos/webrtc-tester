df172d9768ffe04abf31127bcd9236d88bb463d7
megos <gmegos.27@gmail.com>
2020-08-25 21:06:20 +0900

    Apply Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
 
 interface AppProps {}
 
 function App({}: AppProps) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            News
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
