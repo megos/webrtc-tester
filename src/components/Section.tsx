b8e347ec91cb48710be2c4ccd89c07f693d8b8bc
megos <gmegos.27@gmail.com>
2023-04-08 11:24:23 +0900

    Replace makeStyles to styled

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,24 +1,19 @@
 import React from 'react'
-import { Typography, Card, CardContent, Theme } from '@mui/material';
+import { Typography, Card, CardContent, styled } from '@mui/material'
 
-import makeStyles from '@mui/styles/makeStyles';
-
-export const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    margin: theme.spacing(3),
-  },
+const StyledCard = styled(Card)(({ theme }) => ({
+  margin: theme.spacing(3),
 }))
 
 export const Section: React.FC<{ title: string }> = ({ title, children }) => {
-  const classes = useStyles()
   return (
     <>
-      <Typography variant="h6" className={classes.root}>
+      <Typography variant="h6" margin={3}>
         {title}
       </Typography>
-      <Card className={classes.root}>
+      <StyledCard>
         <CardContent>{children}</CardContent>
-      </Card>
+      </StyledCard>
     </>
   )
 }
